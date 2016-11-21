440a7e5f09cd83614a993fe10ebe5bd53de6070a
Ageir9 <Ageir9@users.noreply.github.com>
2016-11-21 13:58:39 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>/iVjLZDDc</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="dist/semantic.min.css">
+        <link rel="stylesheet" type="text/css" href="ct.css">
+    </head>
+    <body>
+        <h1>allah</h1>
+        <table id="t101">
+        <tr>
+            <th>Temperature (°C)</th>
+            <th>Humidity (%)</th> 
+            <th>CO level (ppm)</th>
+        </tr>
+        <tr>
+            <?php
+            $CO_value = import_my_stuff;
+            $TEMP_value = import_my_stuff;
+            $HUMIDITY_value = import_my_stuff;
+            echo "<td>" . $TEMP_value "</td> <td>" . $HUMITIDY_value . "</td> <td>" . $CO_value;
+            >
+            <td>ur</td>
+            <td>gay<td> 
+            pwnd
+        </tr>
+        </table>
+    </body>
+</html> 
