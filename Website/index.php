36806368765ae5bf4bac17e7002b5e9243538c8c
Ageir9 <Ageir9@users.noreply.github.com>
2016-11-29 12:11:06 +0000

    php

--- Website/index.php
+++ Website/index.php
@@ -4,17 +4,12 @@
     <title>/iVjLZDDc</title>
         <meta charset="utf-8">
         <meta name="theme-color" content="#ffffff">
-        <!--favicon-->
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="favicon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
         <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
         <link rel="manifest" href="/manifest.json">
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-        <!--libraries-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.min.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="dist/semantic.min.css">
         <link rel="stylesheet" type="text/css" href="ct.css">
     </head>
     <body>
@@ -22,6 +17,7 @@
         <table id="t101">
         <tr>
             <th>Date</th>
+            <th>Time</th>
             <th>Humidity (%)</th> 
             <th>Temperature (°C)</th>
             <th>CO level (ppm)</th>
@@ -42,20 +38,20 @@
                 echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
                 exit;
             }
+            $createtable = "CREATE TABLE IF NOT EXISTS" //KLARAÐU ÞETTA FRAMTIÐAR ÉG, TAKK
 
             // SQL connect
             $con = mysqli_connect($db_server, $db_username, $db_password, $db_name);
 
             // Execute the query (the recordset $rs contains the result)
-            $sqlresult = mysqli_query($con, "SELECT * FROM value");
+            $result = mysqli_query($con, "SELECT * FROM value");
 
-            // Loop the recordset $sqlresult
+            // Loop the recordset $result
             // Each row will be made into an array ($row) using mysql_fetch_array
-            while($row = mysqli_fetch_array($sqlresult)) {
-
+            while($row = mysqli_fetch_array($result)) {
                 // Write the value of the column 'id' (which is now in the array $row)
-                foreach($sqlresult as $value){
-                    echo "<tr><td>" . $value['Date']. "<td>" . $value['HUMIDITY'] . "<td>" . $value['TEMP'] . "<td>" . $value['CO'] . "<td>" . $value['id'] . "</td><tr>";
+                foreach($result as $value){
+                    echo "<tr><td>" . $value['Date'] . "<td>" . $value['Tími'] . "<td>" . $value['HUMIDITY'] . "<td>" . $value['TEMP'] . "<td>" . $value['CO'] . "<td>" . $value['id'] . "</td><tr>";
                 }
             }
             //loka töflunni
